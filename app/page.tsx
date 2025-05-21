30fdc0d972bf45df66d2bb78b3e065515174df55
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-21 11:54:48 +0900

    ツイート全件表示機能の実装

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,23 @@ import Header from './_components/Header';
 import Footer from './_components/Footer';
 import { TweetData } from './_interfaces/TweetData';
 import TweetList from './_components/TweetList';
+import { useEffect, useState } from 'react';
+import { findAllTweets } from './api/tweets';
 
 const IndexPage = () => {
-  const tweets: TweetData[] = []
+  const [tweets, setTweets] = useState<TweetData[]>([])
+  useEffect(() => {
+    const getTweets = async () => {
+      try {
+        const response = await findAllTweets()
+        setTweets(response)
+      } catch (error) {
+        console.error("ツイートの取得に失敗しました", error)
+      }
+    }
+    getTweets()
+  }, [])
+
   return (
     <div>
       <Header />
