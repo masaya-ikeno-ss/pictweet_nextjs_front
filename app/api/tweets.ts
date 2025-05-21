e6a9dc67cbce9912a60a69700a985abe392f6280
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-21 17:28:40 +0900

    ツイート投稿機能の実装

--- app/api/tweets.ts
+++ app/api/tweets.ts
@@ -2,7 +2,8 @@ import axios from "axios"
 import { TweetData } from "../_interfaces/TweetData"
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "http://localhost:8080/api",
+  withCredentials: true
 })
 
 export const findAllTweets = async (): Promise<TweetData[]> => {
@@ -15,5 +16,19 @@ export const findAllTweets = async (): Promise<TweetData[]> => {
       throw new Error("ツイートの取得に失敗しました")
     }
     throw error
+  }
+}
+
+export const createTweet = async (tweetForm: { image: string; text: string; }): Promise<TweetData> => {
+  try {
+    const response = await api.post('/tweets/', tweetForm);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('APIリクエストエラー:', error.response?.data);
+      const messages = error.response?.data?.messages;
+      throw new Error(messages ? messages.join(', ') : 'ツイートの作成に失敗しました');
+    }
+    throw error;
   }
 }
