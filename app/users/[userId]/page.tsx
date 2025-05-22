7c91b89e63c752d81b2834901edd69f04d27a9aa
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-22 14:31:06 +0900

    マイページのツイート一覧表示とツイート削除機能の実装

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,17 +1,56 @@
 'use client'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Header from '@/app/_components/Header'
 import Footer from '@/app/_components/Footer'
 import TweetList from '@/app/_components/TweetList'
 import { TweetData } from '@/app/_interfaces/TweetData'
 import { UserData } from '@/app/_interfaces/UserData'
+import { notFound, useParams, useRouter } from "next/navigation"
+import { findUserTweets } from "@/app/api/users"
+import { deleteTweet } from "@/app/api/tweets"
 
 const UserMyPage = () => {
-  const [user, setUser] = useState<UserData>({id: 0, nickname: ""})
+  const [user, setUser] = useState<UserData | null>(null)
   const [tweets, setTweets] = useState<TweetData[]>([])
+  const params = useParams()
+  const [loading, setLoading] = useState(true)
+  const userId = params.userId
+  const router = useRouter()
+
+  useEffect(() => {
+    const getTweets = async () => {
+      setLoading(true)
+      if (userId) {
+        try {
+          const response = await findUserTweets(Number(userId))
+          setUser({ id: Number(response.id), nickname: String(response.nickname) })
+          setTweets(response.tweets)
+        } catch (error) {
+          setUser(null)
+          setTweets([])
+          console.error("ユーザー情報の取得に失敗しました", error)
+        } 
+      }
+      setLoading(false)
+    }
+    getTweets()
+  }, [userId])
 
   const handleDeleteTweet = async (tweetId: number) => {
-    // 削除ボタンが押された時の処理
+    try {
+      await deleteTweet(tweetId)
+      router.push("/")
+    } catch (error) {
+      console.log("ツイートの削除に失敗しました", error)
+    }
+  }
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!user) {
+    return notFound()
   }
 
   return (
